d61d3ae842b461ade814279a7dbb685e84fb4b4f
Alex Soler <alexsolerbaca14@gmail.com>
2024-11-29 13:40:03 +0000

    feat: add game functionality

--- number_guess.sh
+++ number_guess.sh
@@ -3,6 +3,32 @@
 
 PSQL="psql --username=postgres --dbname=number_guess -t --no-align -c"
 
+GAME() {
+    NUMBER_OF_GUESSES=0
+    while true 
+    do
+        read GUESS
+        ((NUMBER_OF_GUESSES++))
+
+        if ! [[ $GUESS =~ ^[0-9]+$ ]]
+        then
+            echo "That is not an integer, guess again:"
+        elif [[ $GUESS -lt $RANDOM_NUM ]]
+        then    
+            echo "It's higher than that, guess again."
+        elif [[ $GUESS -gt $RANDOM_NUM ]]
+        then    
+            echo "It's lower than that, guess again."
+        else
+            echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $RANDOM_NUM."
+            # Update user stats in the database
+            USER_STATS_INSERT=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = LEAST(best_game, $NUMBER_OF_GUESSES) WHERE username='$USERNAME';")
+            
+        fi
+    done
+    
+}
+
 RANDOM_NUM=$((1 + $RANDOM % 1000))
 
 echo -e "\nEnter your username:"
@@ -15,10 +41,14 @@ if [[ -z $USER_DATA ]]
 then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
     # Insert new user to database
-    USER_INSERT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME'))")
+    USER_INSERT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
 else
     echo $USER_DATA | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME 
     do  
       echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
     done
 fi
+
+echo -e "\nGuess the secret number between 1 and 1000:"
+GAME
+
